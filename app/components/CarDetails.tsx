96efda8e778c65bcefc94f2e7f80ab496daf309f
Asharf Osama <2300109@dhic.edu.eg>
2026-08-16 11:53:37 +0300

    Refactor code structure for improved readability and maintainability

--- app/components/CarDetails.tsx
+++ app/components/CarDetails.tsx
@@ -1,21 +1,68 @@
 "use client";
+
+import { Fragment } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
 import { CarProps } from "../types";
 import Image from "next/image";
-import { Fragment } from "react/jsx-runtime";
-import { Dialog, Transition } from "@headlessui/react";
 
 interface CarDetailsProps {
   isOpen: boolean;
   closeModal: () => void;
   car: CarProps;
 }
+
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className=" relative z-10 " onClose={closeModal}>
-        <Transition.Child>
-          <div className="fixed inset-0 bg-black  bg-opacity-25" />
-        </Transition.Child>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <TransitionChild
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-sky-300/25" />
+        </TransitionChild>
+        <div className="fixed inset-0 overflow-y-auto ">
+          <div className="flex min-h-full justify-center p-4 text-center items-center">
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
+                <button
+                  type="button"
+                  onClick={closeModal}
+                  className="top-2 absolute right-2  p-2  z-10 w-fit  bg-white rounded-full"
+                >
+                  <Image    
+                    src="/close.svg"
+                    width={30}
+                    alt="close"
+                    height={30}
+                    className="object-contain"
+                  />
+                </button>
+                <div className="flex-1 flex flex-col gap-3">
+                  <div className="w-full relative h-40 bg-pattern bg-cover bg-center rounded-lg"></div>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
       </Dialog>
     </Transition>
   );
